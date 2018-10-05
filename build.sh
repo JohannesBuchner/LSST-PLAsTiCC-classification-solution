d4ca392b1861b9248cc1d5062daf402adfb6e707
Johannes Buchner <johannes.buchner.acad@gmx.com>
2018-10-05 10:51:36 -0300

    speeding up scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #make -j30 $(for i in $(seq 0 99); do echo parts/test_set_part${i}_std_features.txt; done)
 renice +20 $$
-make -j20 $(for i in $(seq 50 99); do echo parts/{test,training}_set_part${i}_{std,slope}_features.txt; done)
+make -j20 $(for i in $(seq 0 99); do echo parts/{test,training}_set_part${i}_{std,slope}_features.txt; done)
