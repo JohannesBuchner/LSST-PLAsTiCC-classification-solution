7c416062850671e52c000a3b31f6bbaa5f5e6bb2
Johannes Buchner <johannes.buchner.acad@gmx.com>
2018-10-12 09:59:14 -0300

    made flat outlier contribution a parameter

--- blend_submission_outliers.py
+++ blend_submission_outliers.py
@@ -3,11 +3,12 @@ Combine classifier predictions with outlier prediction
 
 Parameters:
 
-PRIOR_STRENGTH		weight of prior
+PRIOR_STRENGTH		weight of prior (0: only take predictor, 1: only take prior)
 PRIOR_FLATNESS		prior flatness (1: scale with training distribution, 0: flat over classes)
 SPECZERR		fraction of wrong gal/exgal classification
 EXPO			exponent on classifier probability
-OUTLIER_CONF		strength of outlier votes
+OUTLIER_CONF		strength of outlier votes (0: no outliers marked, 1: detected outliers are marked as probable as all non-outlier classes together, 3: outliers are marked as 3x as probable as all non-outlier classes together)
+PRIOR_STRENGTH_OUTLIERS	additional outlier probability floor (0: take outlier detection directly)
 
 
 """
@@ -18,24 +19,31 @@ import pandas
 
 flatness = float(os.environ.get('PRIOR_FLATNESS', '1.0'))
 
+def flatten(N, flatness):
+	return numpy.where(N == 0, 0, N**flatness)
+	if flatness >= 0:
+		return N**(flatness + 1e-30)
+	else:
+		return numpy.where(N == 0, 0, N**flatness)
+
 data_input = pandas.read_csv('training_set_metadata.csv')
 classes = [6,15,16,42,52,53,62,64,65,67,88,90,92,95,99]
 N = numpy.array([(data_input.target == cl).sum() for cl in classes], dtype='f')
-N = N**(flatness + 1e-30)
+N = flatten(N, flatness)
 N[-1] = N[N>0].min()
 Ntotal = N.sum()
 
-galmask = data_input.hostgal_specz == 0
+galmask = data_input.hostgal_photoz == 0
 Ngal = numpy.array([(numpy.logical_and(data_input.target == cl, galmask)).sum() for cl in classes], dtype='f')
 print("gal   classes:  ", numpy.asarray(classes)[Ngal>0], numpy.where(Ngal>0))
-Ngal = Ngal**(flatness + 1e-30)
+Ngal = flatten(Ngal, flatness)
 Ngal[-1] = Ngal[Ngal>0].min()
 Ntotal_gal = Ngal.sum()
 
 exgalmask = ~galmask
 Nexgal = numpy.array([(numpy.logical_and(data_input.target == cl, exgalmask)).sum() for cl in classes], dtype='f')
 print("exgal classes:  ", numpy.asarray(classes)[Nexgal>0], numpy.where(Nexgal>0))
-Nexgal = Nexgal**(flatness + 1e-30)
+Nexgal = flatten(Nexgal, flatness)
 Nexgal[-1] = Nexgal[Nexgal>0].min()
 Ntotal_exgal = Nexgal.sum()
 
@@ -53,6 +61,7 @@ df = pandas.read_csv(sys.argv[1])
 assert (df.object_id == supp_df.object_id).all(), (df.object_id, supp_df.object_id)
 del supp_df
 w = float(os.environ.get('PRIOR_STRENGTH', '0.0'))
+w_outliers = float(os.environ.get('PRIOR_STRENGTH_OUTLIERS', '0.0'))
 w_wrongspecz = float(os.environ.get('SPECZERR', '0.01'))
 expo = float(os.environ.get('EXPO', '1.0'))
 
@@ -65,33 +74,46 @@ outlier_confidence = float(os.environ.get('OUTLIER_CONF', '1.0'))
 outlier_votes = 0
 nvotes_total = 0
 for outliertechnique in 'EllEnvelope', 'IsolForest':
-	for thresh in 0.001, 0.01, 0.04:
+	for thresh in 0.001, 0.01, 0.04, 0.1, 0.4:
 		print('loading outlier votes of %s[%s] ...' % (outliertechnique, thresh))
 		is_outlier = -1 == numpy.loadtxt('test_set_all_sorted.csv.gz_novel_%s-%s.csv' % (outliertechnique, thresh))
 		outlier_votes = outlier_votes + is_outlier * outlier_voteweights[outliertechnique]
 		nvotes_total += 1
 
+total_probs = 0
 
 for col, Ni, Ngali, Nexgali in zip(df.columns[1:], N, Ngal, Nexgal):
 	print('  adjusting column "%s" ...' % (col))
 	if col == 'class_99':
 		# novelty class: the other probabilities should sum to 1
-		predictor = outlier_votes * outlier_confidence / nvotes_total
+		# here we add outlier vote as a factor relative to the other probabilities
+		predictor = total_probs * outlier_votes * outlier_confidence / nvotes_total
+		w_prior = w_outliers
+		
+		col_prob = predictor + w_prior
 	else:
+		w_prior = w
 		predictor = df.loc[:,col]**expo
+		
+		prior_gal = Ngali * 1. / Ntotal_gal
+		prior_exgal = Nexgali * 1. / Ntotal_exgal
+		prior = Ni * 1. / Ntotal
+		
+		# blend in the prior
+		prior = w_wrongspecz * numpy.where(~galmask, prior_gal, prior_exgal) + (1 - w_wrongspecz) * numpy.where(galmask, prior_gal, prior_exgal)
 	
-	prior_gal = Ngali * 1. / Ntotal_gal
-	prior_exgal = Nexgali * 1. / Ntotal_exgal
-	prior = Ni * 1. / Ntotal
+		col_prob = predictor * (1 - w_prior) + prior * w_prior
 	
-	# blend in the prior
-	prior = w_wrongspecz * numpy.where(~galmask, prior_gal, prior_exgal) + (1 - w_wrongspecz) * numpy.where(galmask, prior_gal, prior_exgal)
-	
-	df.loc[:,col] = predictor * (1 - w) + prior * w
+	total_probs = total_probs + col_prob
+	df.loc[:,col] = col_prob
+
+print('normalising columns ...')
+for col in df.columns[1:]:
+	df.loc[:,col] /= total_probs
 
 print("writing data ...")
 #data1.values[:,1:] = data1.values[:,1:]**2 * (1 - w) + (N * 1. / Ntotal).reshape((1,-1)) * w
-df.to_csv(sys.argv[1] + '_blend_expo%s_noveltyconf%s_z%s_prior%sflat%s.csv.gz' % (expo, outlier_confidence, w_wrongspecz, w, flatness), 
+df.to_csv(sys.argv[1] + '_blend_expo%s_noveltyconf%sprior%s_z%s_prior%sflat%s.csv.gz' % (expo, outlier_confidence, w_outliers, w_wrongspecz, w, flatness), 
 	float_format='%.3e', index=False, header=True, compression='gzip', chunksize=100000)
 
 
